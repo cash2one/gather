39a5eeca428dcee66ced4977913484c28c405134
zhangbo <zhangbo@cfpamf.org.cn>
2015-01-05 23:59:15 +0800

    添加主页shout out功能

--- gather/gather/views.py
+++ gather/gather/views.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+
+from bookmark.models import NotePad
 
 from config.models import IndexImg, IndexText
 from config.decorators import click_log
@@ -11,9 +14,21 @@ from config.decorators import click_log
 @click_log
 def index(request, template_name='index.html'):
     """ 主页显示"""
-    imgs = IndexImg.objects.filter(is_show=True).order_by('ordering')[:1]
-    texts = IndexText.objects.filter(is_show=True)[:1]
+    if request.method == "POST":
+        if request.user.is_authenticated():
+            if request.POST.get('note'):
+                title = request.POST.get('note')
+                if not NotePad.objects.filter(title=title).exists():
+                    NotePad(
+                        user=request.user,
+                        title=title,
+                    ).save()
+            else:
+                messages.error(request, 'shout out不能为空')
+
+        else:
+            messages.info(request, '请登录后才能shout out')
+    nodes = NotePad.objects.all().order_by("created")
     return render(request, template_name, {
-        'imgs': imgs,
-        'texts': texts,
+        'nodes': nodes,
     })
