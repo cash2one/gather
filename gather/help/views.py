6600f6f43120e6bb2d55dc678aa74ff6f602fd7c
zhangbo <zhangbo@cfpamf.org.cn>
2015-03-27 13:43:24 +0800

    修复弹出框中的x轴滚轮

--- gather/help/views.py
+++ gather/help/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-@login_required
 def help(request, template_name='help/index.html'):
     """ 地图展示"""
     return render(request, template_name, {
