08b287d6392693c2e7f0acf5bfeedf1549774328
jacsice <jacsicee@gmail.com>
2015-10-27 10:38:32 +0800

    unknown error on chatting

--- gather/gather/settings.py
+++ gather/gather/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = (
     'chatting',
     'wash',
     'wechat',
-    'djcelery'
+    'djcelery',
     # 'debug_toolbar',
 
 )
--- gather/main.py
+++ gather/main.py
@@ -13,7 +13,7 @@ from tornado.options import define, options, parse_command_line
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gather.settings")
 
-from chatting.chat import MainHandler, MessageNewHandler, MessageUpdatesHandler
+#from chatting.chat import MainHandler, MessageNewHandler, MessageUpdatesHandler
 
 define("port", default=8000, help="run on the given port", type=int)
 
@@ -25,9 +25,9 @@ def main():
 
     app = tornado.web.Application(
         [
-            (r"/chat", MainHandler),
-            (r"/chat/message/new", MessageNewHandler),
-            (r"/chat/message/updates", MessageUpdatesHandler),
+            #(r"/chat", MainHandler),
+            #(r"/chat/message/new", MessageNewHandler),
+            #(r"/chat/message/updates", MessageUpdatesHandler),
             (r'.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
         ],
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
